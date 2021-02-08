dcfb28c3a43144ec9e10da638e0642db3cb563eb
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-08 00:19:25 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,8 +38,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  matrix = matrix.sort![row][column]
-  return matrix
+  matrixx = matrix.sort![row][column]
+  return matrixx
 end
 
 def matrix_update(matrix, row, column, new_value)
